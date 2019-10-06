148e20a0105888867f21510ebc4f60a2271cca69
FearTheRogue <43548013+FearTheRogue@users.noreply.github.com>
2019-10-06 13:41:07 +0100

    Added Ground & Character

---
+++ Week2_Activities/Assets/CharacterMovements.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovements : MonoBehaviour
+{
+    public Rigidbody rb;
+    public int speed = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxisRaw("Horizontal"))
+        {
+
+        }
+
+
+    }
+}
