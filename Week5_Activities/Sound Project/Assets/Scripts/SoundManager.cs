ceafffa26ca1e298890b2d4c7a8067157f4d8c3f
FearTheRogue <43548013+FearTheRogue@users.noreply.github.com>
2019-10-31 12:36:21 +0000

    Created Sound Testing Scene

---
+++ Week5_Activities/Sound Project/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField]
+    private AudioMixer m_audioMixer;
+
+
+    public void SetMusicVolume(float _volume)
+    {
+        m_audioMixer.SetFloat("AmbientVolume", _volume);
+    }
+}
